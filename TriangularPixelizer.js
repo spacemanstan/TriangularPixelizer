9374474342de756a86a46075618d88b0bf14fd25
spacemanstan <ariston.bodnarchuk@gmail.com>
2024-01-17 15:43:56 -0600

    project initialization
    just a very simple frame to make sure p5js is working and loading with some error messages if it doesn't
    
    it has some basic p5js code to render something of interest

---
+++ TriangularPixelizer.js
@@ -0,0 +1,67 @@
+let img;
+
+let h1;
+let input;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+  h1 = createElement('h1');
+  input = createInput('Input test field');
+  input.size(200, 20);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+
+  let locX = mouseX - height / 2;
+  let locY = mouseY - width / 2;
+
+  ambientLight(60, 60, 60);
+  pointLight(255, 255, 255, locX, locY, 100);
+
+  push();
+  translate(-width / 4, -height / 4, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  fill(250, 0, 0);
+  torus(80, 20, 64, 64);
+  pop();
+
+  push();
+  translate(width / 4, -height / 4, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  normalMaterial();
+  torus(80, 20, 64, 64);
+  pop();
+
+  push();
+  translate(-width / 4, height / 4, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  ambientMaterial(250);
+  torus(80, 20, 64, 64);
+  pop();
+
+  push();
+  translate(width / 4, height / 4, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  specularMaterial(250);
+  torus(80, 20, 64, 64);
+  pop();
+
+  textSize(20);
+  text(input.value(), 50,100);
+  
+  h1.html( input.value() );
+}
